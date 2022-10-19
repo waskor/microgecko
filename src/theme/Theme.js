ac9a26c914672d0cb7c3a6fa1f6ae027eb60a941
vas <wkorobitsyn0@gmail.com>
2022-10-19 13:02:07 +0200

    meta

--- src/theme/Theme.js
+++ src/theme/Theme.js
@@ -2,17 +2,10 @@ import { extendTheme } from "@chakra-ui/react";
 import { mode } from "@chakra-ui/theme-tools";
 
 const config = {
-  initialColorMode: "light",
+  initialColorMode: "dark",
   useSystemColorMode: false,
-  styles: {
-    global: (props) => ({
-      body: {
-        bg: mode("#000000", "#000000")(props),
-      },
-    }),
-  },
 };
 
-const theme = extendTheme({ config });
+const theme = extendTheme(config);
 
 export default theme;
